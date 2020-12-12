18b8fa5fb3abab7eadcd6186d24e867e49855ed3
nickd9xp <75902070+nickd9xp@users.noreply.github.com>
2020-12-12 09:54:05 -0600

    Add files via upload

---
+++ MathExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonlyMissingFunctions
+{
+    public static class MathExtension
+    {
+        public static int Modulo (int a, int n)
+        {
+            return a - (int)Math.Floor((double)a / n) * n;
+        } 
+    }
+}
