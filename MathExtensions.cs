a8526cafa9fbfd9a3f814867bc7ae8c17c099ab0
nickd9xp <75902070+nickd9xp@users.noreply.github.com>
2022-07-16 10:36:40 -0500

    Update and rename MathExtension.cs to MathExtensions.cs

---
+++ MathExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CommonlyMissingFunctions
+{
+    public static class MathExtensions
+    {
+        public static int Modulo (this int a, int n)
+        {
+            return (a - (int)Math.Floor((double)a / n) * n) % n;
+        }
+
+        public static int Grid (this int a, int n)
+        {
+            return (a / n).Modulo(n);
+        }
+    }
+}
